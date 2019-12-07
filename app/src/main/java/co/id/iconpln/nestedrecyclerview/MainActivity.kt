f56cb8d8580b7ff5076fd5c202f09bdbf4585b86
Rahmat Kurniawan <rahmat.kurniawan.iconpln@gmail.com>
2019-12-07 19:27:04 +0700

    MainActivity integrate recyclerView adapter and view

--- app/src/main/java/co/id/iconpln/nestedrecyclerview/MainActivity.kt
+++ app/src/main/java/co/id/iconpln/nestedrecyclerview/MainActivity.kt
@@ -2,11 +2,25 @@ package co.id.iconpln.nestedrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.id.iconpln.nestedrecyclerview.Data.Menu
+import co.id.iconpln.nestedrecyclerview.adapter.MenuAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var menuData = listOf<Any>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        menuData = Menu.menus
+
+        val menuAdapter = MenuAdapter(menuData)
+
+        menuItem.hasFixedSize()
+        menuItem.layoutManager = LinearLayoutManager(this)
+        menuItem.adapter = menuAdapter
     }
 }
