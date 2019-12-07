91a718d1748c199d920ce16f53300042e07635f6
Rahmat Kurniawan <rahmat.kurniawan.iconpln@gmail.com>
2019-12-07 18:28:10 +0700

    add companion object for different view type

--- app/src/main/java/co/id/iconpln/nestedrecyclerview/adapter/MenuAdapter.kt
+++ app/src/main/java/co/id/iconpln/nestedrecyclerview/adapter/MenuAdapter.kt
@@ -4,16 +4,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class MenuAdapter(private val data: List<Any>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object{
+        private const val ITEM_HEADER = 0
+        private const val ITEM_MENU = 1
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
