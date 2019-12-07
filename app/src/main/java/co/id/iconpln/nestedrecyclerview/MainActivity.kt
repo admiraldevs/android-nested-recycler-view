fd6086c90e51c80c7cf1b24d6f3d1ec94908596a
Rahmat Kurniawan <rahmat.kurniawan.iconpln@gmail.com>
2019-12-07 20:02:23 +0700

    add content add and remove order in MainActivity

--- app/src/main/java/co/id/iconpln/nestedrecyclerview/MainActivity.kt
+++ app/src/main/java/co/id/iconpln/nestedrecyclerview/MainActivity.kt
@@ -10,17 +10,56 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var menuData = listOf<Any>()
+    private var totalOrder = 0
+    private var totalPrice = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        menuData = Menu.menus
+        menuData = Menu.menus.toMutableList()
 
         val menuAdapter = MenuAdapter(menuData)
+        menuAdapter.setAddedItemListener { menuItem, position ->
+            val itemCount = menuItem.count
+            val menu = menuItem.copy()
+            menu.count = itemCount + 1
 
-        menuItem.hasFixedSize()
-        menuItem.layoutManager = LinearLayoutManager(this)
-        menuItem.adapter = menuAdapter
+            (menuData as MutableList<Any>)[position] = menu
+            menuAdapter.notifyItemChanged(position, menu)
+
+            totalPrice += menu.price
+            totalOrder++
+
+            tvReviewBooking.text = String.format(getString(R.string.review_booking))
+        }
+
+        menuAdapter.setRemoveItemListener { menuItem, position ->
+            val itemCount = menuItem.count
+            val menu = menuItem.copy()
+            menu.count = itemCount - 1
+
+            (menuData as MutableList<Any>)[position] = menu
+            menuAdapter.notifyItemChanged(position, menu)
+
+            totalPrice -= menu.price
+            totalOrder--
+
+            if (totalOrder > 0) {
+                tvReviewBooking.text = String.format(getString(R.string.review_booking), totalOrder, totalPrice)
+            } else {
+                tvReviewBooking.text = "-"
+            }
+        }
+
+        menuItem.apply {
+            hasFixedSize()
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = menuAdapter
+        }
+
+        // menuItem.hasFixedSize()
+        // menuItem.layoutManager = LinearLayoutManager(this)
+        // menuItem.adapter = menuAdapter
     }
 }
